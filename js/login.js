0d812506893997c82b6166382ee3fa2d6374e2aa
muntasir-mahmud-abdullah <muntasir525@gmail.com>
2024-10-13 11:24:06 +0600

    home.html file and login.js addend

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinCode = document.getElementById("pin-code").value;
+    console.log(phoneNumber, pinCode);
+    if (phoneNumber === "12345" && pinCode == "1234") {
+      console.log("You are logged in ");
+      window.location.href = "/index.html";
+    } else {
+      alert("wrong phone number or password");
+    }
+  });
